c569cfefbe2949ac0e1f1925dcbe005004a1d240
DroidYe <yeliheng00@163.com>
2020-02-15 19:43:03 +0800

    VRTK控制器事件模板

---
+++ VRTKControllerEventsTemplate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+public class VRTKControllerEventsUtil : MonoBehaviour
+{
+
+    public VRTK_ControllerEvents controllerEvents;
+
+    //注册事件
+    void OnEnable() {
+        controllerEvents.TriggerPressed += ControllerEvents_TriggerPressed;
+    }
+
+    //注销
+    void OnDisable() {
+        controllerEvents.TriggerPressed -= ControllerEvents_TriggerPressed; 
+    }
+
+    //事件详细处理逻辑....
+    void ControllerEvents_TriggerPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        print("Trigger Pressed !!");
+    }
+}
